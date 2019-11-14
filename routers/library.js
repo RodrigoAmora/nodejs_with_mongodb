f3ccfc25be9b9b5714e5ac41dda22bd798b9e75a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2019-11-14 20:29:01 -0300

    Creating list the route nearbyLibraries

--- routers/library.js
+++ routers/library.js
@@ -12,5 +12,33 @@ router.get("/", (req, res) => {
    });
 })
 
+router.post('/nearbyLibraries', (req, res) => {
+	var db = require("../db");
+
+	var lat = req.body.lat;
+	var lng = req.body.lng;
+
+	console.log(req.body)
+	var query = {
+	    location: {
+	      $near: {
+	        $geometry: {
+	          type: 'Point',
+	          coordinates: [lng, lat]
+	        },
+	        $maxDistance: 3000
+	      }
+	    }
+	}
+	
+	var Library = db.Mongoose.model('library', db.LibrarySchema);
+	Library.find(query, (err, results) => {
+		if (err) {
+			console.log("Error!!!!")
+		} else {
+			res.render('library.ejs', { data: results })
+		}
+	})
+});
 
 module.exports = router;
